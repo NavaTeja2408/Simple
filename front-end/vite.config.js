86c7d905c497d553f231d0382f6a39c9ba95a55d
Shole <navatejapendiala@Shole.local>
2025-03-03 15:42:36 +0530

    first

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    visualizer(), // Generates a bundle analysis report
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increases chunk size warning limit (default is 500KB)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.split("node_modules/")[1].split("/")[0]; // Creates separate chunks for dependencies
+          }
+        },
+      },
+    },
+  },
+});
