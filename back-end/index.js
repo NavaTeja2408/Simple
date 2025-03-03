4551987cc4cd175ba0c45c49bf97d36ed7474cf3
Shole <navatejapendiala@Shole.local>
2025-03-03 16:08:07 +0530

    first

--- back-end/index.js
+++ back-end/index.js
@@ -4,13 +4,18 @@ const mongoose = require("mongoose");
 const authRouter = require("./Routes/authRouter");
 const workspace = require("./Routes/workspaceRouter");
 const editor = require("./Routes/editorRouter");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://simplefront-iota.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://simplefront-iota.vercel.app",
+      "http://localhost:9000",
+    ],
   })
 );
 app.use(express.urlencoded({ extended: false }));
@@ -26,11 +31,11 @@ mongoose
     console.log("Database is not Connected");
   });
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Server has been created",
-  });
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../front-end/dist", "index.html"));
 });
+
 app.use("/api/auth", authRouter);
 app.use("/api/workspace", workspace);
 app.use("/api/editor", editor);
