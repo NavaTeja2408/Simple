195f752d847cfcb3fe1e72058e5b224a1dc6a823
Shole <navatejapendiala@Shole.local>
2025-03-18 17:28:38 +0530

    nine

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import WebsiteRoute from "./routes/WebsiteRoute";
 
 function App() {
--- front-end/src/context/UserContext.jsx
+++ front-end/src/context/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 
 export const UserContext = createContext({});
