25baf98e92f0d44ca6ab7dfefd42bce2cd56241d
Shole <navatejapendiala@Shole.local>
2025-05-20 11:28:56 +0530

    fifth

--- front-end/src/pages/Editor/Slate/CodeBlock.jsx
+++ front-end/src/pages/Editor/Slate/CodeBlock.jsx
@@ -1,26 +1,6 @@
 import React, { useEffect } from "react";
-import MonacoEditor from "@monaco-editor/react";
 
 const CodeBlock = ({ content, onChange, preview }) => {
-  useEffect(() => {
-    if (window.monaco && window.monaco.editor) {
-      window.monaco.editor.defineTheme("myCustomTheme", {
-        base: "vs", // base theme (vs for light, vs-dark for dark)
-        inherit: true,
-        rules: [],
-        colors: {
-          "editor.background": "#E8E8E8", // Set grey background
-        },
-      });
-
-      // Set the custom theme
-      window.monaco.editor.setTheme("myCustomTheme");
-    } else {
-      // If Monaco is not loaded yet, log or handle error
-      console.log("Monaco editor not yet loaded");
-    }
-  }, []);
-
   // Inline styles for the editor container
   const containerStyles = {
     backgroundColor: "#E8E8E8", // Set grey background
