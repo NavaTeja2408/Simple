48e1a4c18966fd65646334fd4bbd8d69822e17a7
Shole <navatejapendiala@Shole.local>
2025-07-24 12:05:57 +0530

    eleven

--- front-end/src/context/DatabaseContext.jsx
+++ front-end/src/context/DatabaseContext.jsx
@@ -3,8 +3,8 @@ import React, { createContext, useContext } from "react";
 export const DatabaseContext = createContext({});
 
 export function DatabaseContextProvider({ children }) {
-  // const databaseUrl = "https://simple-jet-eta.vercel.app";
-  const databaseUrl = "http://localhost:9000";
+  const databaseUrl = "https://simple-jet-eta.vercel.app";
+  // const databaseUrl = "http://localhost:9000";
 
   return (
     <DatabaseContext.Provider
